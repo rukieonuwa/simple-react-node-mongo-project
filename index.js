3ab9e0570d93f962eba2131392e7a0aa61e557bf
Rukevwe Onuwa <rukieonuwa@gmail.com>
2022-01-28 19:52:49 +0100

    used process.env to store passwords

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const customerRouter = require('./routes/customerRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const mongoDbUser = process.env.MONGO_USER
+const mongoDbPassword = process.env.MONGO_PASSWORD
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://rukevwe:rukevwe@customerdb.yvvra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    await mongoose.connect(`mongodb+srv://${mongoDbUser}:${mongoDbPassword}@customerdb.yvvramongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
     console.log('successfully connected to mongodb');
   } catch (err) {
     console.log(err.message);
