29e4a8bdbc9358b1b321ce4c2ebdf108e12d5b40
Rukevwe Onuwa <rukieonuwa@gmail.com>
2022-01-28 21:07:45 +0100

    installed dotenv

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const joi = require('joi');
+require('dotenv').config(); 
 
 const customerRouter = require('./routes/customerRoutes');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const mongoDbUser = process.env.MONGO_USER
-const mongoDbPassword = process.env.MONGO_PASSWORD
+const PORT = process.env.PORT || 3009;
+const mongoDbUser = process.env.MONGO_USER;
+const mongoDbPassword = process.env.MONGO_PASSWORD;
+const mongostring = `mongodb+srv://${mongoDbUser}:${mongoDbPassword}@customerdb.yvvra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 (async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${mongoDbUser}:${mongoDbPassword}@customerdb.yvvramongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    await mongoose.connect(mongostring)
     console.log('successfully connected to mongodb');
   } catch (err) {
     console.log(err.message);
